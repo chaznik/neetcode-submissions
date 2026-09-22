979a87a3cc44b07dc96c4170c079c7a4ff8cbf1b
Chazni <Chazni.k9@gmail.com>
2026-09-22 22:21:03 +0200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.go
@@ -0,0 +1,20 @@
+func isAnagram(s string, t string) bool {
+	sMap := map[rune]int{}
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	for _, char := range s {
+		sMap[char]++
+	}
+
+	for _, char := range t {
+		sMap[char]--
+
+		if sMap[char] < 0 {
+			return false
+		}
+	}
+	return true
+}
