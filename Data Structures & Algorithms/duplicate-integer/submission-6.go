bcf996f48a36c5ae1a7299cba4f5c96dd3c63972
Chazni <Chazni.k9@gmail.com>
2026-09-22 21:03:48 +0200

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.go
@@ -0,0 +1,12 @@
+func hasDuplicate(nums []int) bool {
+    numDict := make(map[int]int)
+
+    for _, num := range nums {
+        _, exists := numDict[num]
+        if exists {
+            return true
+        }
+        numDict[num] = 1
+    }
+    return false
+}
