40d5140830ea99061375a8f5398d5a07e8642d37
Chazni <Chazni.k9@gmail.com>
2026-09-23 18:42:09 +0200

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    indexMap := make(map[int]int)
+
+    for i, num := range nums {
+        wantedIndex, found := indexMap[target - num]
+        if found {
+            return []int { wantedIndex, i }
+        }
+        indexMap[num] = i
+    }
+    return []int {}
+}
